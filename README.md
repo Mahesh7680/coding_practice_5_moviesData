61499ad6ca40d175ae454aefa6494da6bfc1ac90
mahesh <anantharammahesh05@gmail.com>
2023-07-02 07:52:56 +0000

    9/11

--- README.md
+++ README.md
@@ -184,4 +184,5 @@ Use `npm install` to install the packages.
 **Export the express instance using the default export syntax.**
 
 **Use Common JS module syntax.**
+
 # coding_practice_5_moviesData
--- app.js
+++ app.js
@@ -35,6 +35,8 @@ const convertSnakeCaseIntoCamelCase = (dbObject) => {
   };
 };
 
+//  get all movie names         API - 1
+
 app.get("/movies/", async (request, response) => {
   const getAllMoviesQuery = `
     SELECT
@@ -43,9 +45,95 @@ app.get("/movies/", async (request, response) => {
       movie;
     `;
   const responseObj = await db.all(getAllMoviesQuery);
+  response.send(responseObj.map((each) => convertSnakeCaseIntoCamelCase(each)));
+});
+
+// get one movie name           API - 3
+
+app.get("/movies/:movieId/", async (request, response) => {
+  const { movieId } = request.params;
+  const getAllMoviesQuery = `
+    SELECT
+      *
+    FROM
+      movie
+    WHERE
+      movie_id = ${movieId};
+    `;
+  const responseObj = await db.run(getAllMoviesQuery);
+  response.send(convertSnakeCaseIntoCamelCase(responseObj));
+});
+
+//POST MOVIE DETAILS            API - 2
+
+app.post("/movies/", async (request, response) => {
+  const movieDetails = request.body;
+  const { directorId, movieName, leadActor } = movieDetails;
+  const add_new_movie_query = `
+    INSERT INTO 
+        movie(director_id, movie_name, lead_actor)
+    VALUES
+    (
+        ${directorId},
+        '${movieName}',
+        '${leadActor}'
+    );`;
+  const dbResponse = await db.run(add_new_movie_query);
+  const movieId = dbResponse.lastId;
+  response.send("Movie Successfully Added");
+});
+
+// PUT MOVIE DETAILS        API - 4
+
+app.put("/movies/:movieId/", async (request, response) => {
+  const { movieId } = request.params;
+  const movieDetails = request.body;
+  const { directorId, movieName, leadActor } = movieDetails;
+  const updateMovieQuery = `
+  UPDATE
+    movie
+  SET 
+    director_id = ${directorId},
+    movie_name = '${movieName}',
+    lead_actor = '${leadActor}'
+    WHERE
+    movie_id = ${movieId};
+  `;
+  const dbResponse = await db.run(updateMovieQuery);
+  response.send("Movie Details Updated");
+});
+
+// DELETE MOVIE         API - 5
+
+app.delete("/movies/:movieId/", (request, response) => {
+  const { movieId } = request.params;
+  const deleteMovieQuery = `
+      DELETE FROM 
+        movie
+      WHERE
+        movie_id = ${movieId};
+    `;
+  db.run(deleteMovieQuery);
+  response.send("Movie Removed");
+});
+
+const convertSnakeCaseToCamelCase = (dbObject) => {
+  return {
+    directorId: dbObject.director_id,
+    directorName: dbObject.director_name,
+  };
+};
+
+//  GET DIRECTOR        API - 6
+
+app.get("/directors/", async (request, response) => {
+  const getDirectorsQuery = `
+    SELECT
+      *
+    FROM
+      director;`;
+  const dbResponse = await db.all(getDirectorsQuery);
   response.send(
-    responseObj.map((each) => {
-      convertSnakeCaseIntoCamelCase(each);
-    })
+    dbResponse.map((eachPlayer) => convertSnakeCaseToCamelCase(eachPlayer))
   );
 });
